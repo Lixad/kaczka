18002662ec9502c448bdd028ad91e76f476c37b2
Lixad <filip.wesolowski@onet.eu>
2023-08-28 14:36:41 +0200

    feat: build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,7 +30,7 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   <iframe id="twitch-chat-embed"
-    src="https://www.twitch.tv/embed/rozowakaczuszka/chat?parent=https://localhost:5173/"
+    src="https://www.twitch.tv/embed/rozowakaczuszka/chat?parent=https://lixad.github.io/"
     height="500"
     width="350">
   </iframe>

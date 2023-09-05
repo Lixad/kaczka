f2e7eceac6eb1f19c16a38a43c80c1ef005664be
Lixad <filip.wesolowski@onet.eu>
2023-09-05 14:39:39 +0200

    feat: update chat embed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,7 +30,7 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   <iframe id="twitch-chat-embed"
-    src="https://www.twitch.tv/embed/rozowakaczuszka/chat?parent=https://lixad.github.io/"
+    src="https://www.twitch.tv/embed/rozowakaczuszka/chat?parent=lixad.github.io/"
     height="500"
     width="350">
   </iframe>

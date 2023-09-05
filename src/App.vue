0d2d65607cb71e77898bb8090601c61512003b35
Lixad <filip.wesolowski@onet.eu>
2023-09-05 15:31:26 +0200

    feat: shrek test

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="test">
+    <iframe src="https://ebd.cda.pl/620x368/13620795e7" width="100%" height="100%" style="border:none;" scrolling="no" allowfullscreen name="v2" allow="encrypted-media"></iframe>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <video src="https://ebd.cda.pl/620x368/13620795e7"></video>
+  <div class="player-box">
+    <iframe
+      src="https://player.twitch.tv/?channel=rozowakaczuszka&parent=lixad.github.io"
+      height="400"
+      width="600"
+      allowfullscreen
+      class="player"
+    >
+    </iframe>
+  </div>
+  <div class="chat-box">
+    <iframe
+      id="twitch-chat-embed"
+      src="https://www.twitch.tv/embed/rozowakaczuszka/chat?parent=lixad.github.io"
+      height="100%"
+      width="100%"
+      class="chat"
+    >
+    </iframe>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.test {
+  position: absolute;
+  height: 100vh;
+  width: 80vw;
+  top: 0;
+  left: 0;
+  z-index: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.chat-box {
+  position: absolute;
+  top: 0px;
+  right: 4px;
+  width: 19vw;
+  height: 100vh;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.player-box {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 10;
 }
+
 </style>
